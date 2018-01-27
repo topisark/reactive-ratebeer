a4471d1089c5dd391864ec112b95c01ca5b5d530
topisark <topi.sarkkinen@gmail.com>
2018-01-27 17:16:49 +0200

    Remove service worker: probably a bit unnecessary in this project

--- frontend/config/webpack.config.prod.js
+++ frontend/config/webpack.config.prod.js
@@ -189,23 +189,6 @@ module.exports = {
     new ManifestPlugin({
       fileName: 'asset-manifest.json',
     }),
-    new SWPrecacheWebpackPlugin({
-      dontCacheBustUrlsMatching: /\.\w{8}\./,
-      filename: 'service-worker.js',
-      logger(message) {
-        if (message.indexOf('Total precache size is') === 0) {
-          return;
-        }
-        if (message.indexOf('Skipping static resource') === 0) {
-          return;
-        }
-        console.log(message);
-      },
-      minify: true,
-      navigateFallback: publicUrl + '/index.html',
-      navigateFallbackWhitelist: [/^(?!\/__).*/],
-      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
-    }),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
   ],
   node: {
