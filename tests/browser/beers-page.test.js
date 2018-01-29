2b2ca88a1d86003c61b5c340e7f390ff138b21ca
topisark <topi.sarkkinen@gmail.com>
2018-01-29 21:30:57 +0200

    Simple test for beer page

--- tests/browser/beers-page.test.js
+++ tests/browser/beers-page.test.js
@@ -1,11 +1,15 @@
-import { ClientFunction, Selector } from 'testcafe';
-
-const currentLocation = ClientFunction(() => document.location.href);
+import { Selector } from 'testcafe';
 
 fixture `Beers page`
-  .page `https://reactive-ratebeer.herokuapp.com/beers`;
+  .page `https://reactive-ratebeer.herokuapp.com/`;
 
 test('typing into search box filters beers', async testCafe => {
-  await testCafe
-    .typeText('input[type="text"]', '1234567-8');
+  await testCafe.click(Selector('span').withText('BEERS'));
+
+  const beers = Selector('.beers-content').child();
+  const initialBeersCount = await beers.count;
+  await testCafe.typeText('input[type="text"]', 'foobar-barfoo');
+
+  const currentBeersCount = await beers.count;
+  await testCafe.expect(initialBeersCount).gt(currentBeersCount);
 });
