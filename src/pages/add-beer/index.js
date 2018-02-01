ea695f4ee3ce456df08230dfaa5a74ad3a49d93c
topisark <topi.sarkkinen@gmail.com>
2018-02-01 21:44:57 +0200

    Handle submit (mocked) and show success dialog

--- src/pages/add-beer/index.js
+++ src/pages/add-beer/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { Button, TextField } from 'material-ui';
+import request from 'request-promise';
+import { Button, Dialog, TextField } from 'material-ui';
+import { Link } from 'react-router-dom';
+import { apiUrl } from '../../constants';
 import './add-beer.scss';
 
+function mockPost() {
+  return Promise.resolve();
+}
+
+const beerProperties = ['Name', 'Berwery', 'Description'];
+
+const SuccessDialog = () => (
+  <Dialog open>
+    <div className="add-beer-dialog">
+      <h1>Beer added, yay!</h1>
+      <Link to="/beers">
+        <Button>See the beers</Button>
+      </Link>
+    </div>
+  </Dialog>
+);
+
 // TODO: Array of fields and map them instead of so much repetition?
+// TODO: Validation?
 export default class AddBeerPage extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = { dialogOpen: false };
   }
 
   handleChange = (event) => {
@@ -14,22 +35,37 @@ export default class AddBeerPage extends React.Component {
     this.setState({ [name]: value });
   };
 
+  handleSubmit = () => {
+    const { name, brewery, description } = this.state;
+    const requestOptions = {
+      url: `${apiUrl}/beers/new`,
+      json: true,
+      body: {
+        name,
+        brewery,
+        description
+      }
+    };
+
+    mockPost(requestOptions).then(() => {
+      this.setState({ dialogOpen: true });
+    });
+  };
+
   render() {
     return (
       <div className="add-beer">
+        { this.state.dialogOpen && <SuccessDialog /> }
         <h1>Add a beer!</h1>
         <div className="add-beer-fields">
-          <div className="add-beer-fields-input">
-            <TextField onChange={this.handleChange} name="name" label="Name" />
-          </div>
-          <div className="add-beer-fields-input">
-            <TextField onChange={this.handleChange} name="brewery" label="Brewery" />
-          </div>
-          <div className="add-beer-fields-input">
-            <TextField onChange={this.handleChange} multiline name="description" label="Description" />
-          </div>
+          { beerProperties.map(property => (
+            <div key={property} className="add-beer-fields-input">
+              <TextField onChange={this.handleChange} name={property.toLowerCase()} label={property} />
+            </div>
+          ))
+          }
         </div>
-        <Button raised color="primary">Submit the beer!</Button>
+        <Button raised onClick={this.handleSubmit} color="primary">Submit the beer!</Button>
       </div>
     );
   }
