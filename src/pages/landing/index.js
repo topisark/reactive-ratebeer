0fc0d1bf6b037b4a444f54c237ecb365d3fd26b7
topisark <topi.sarkkinen@gmail.com>
2018-02-01 20:27:21 +0200

    Created a rather shitty landing page

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Paper } from 'material-ui';
+import './landing.scss';
 
 export default class LandingPage extends React.Component {
   render() {
     return (
-      <div>
-        Landing page
+      <div className="landing-page">
+        <Paper className="landing-page-content">
+          <h1>Hello!</h1>
+          <p>Well, welcome to Ratebeer! I warn you there's not much to see right now, though.</p>
+        </Paper>
       </div>
     );
   }
