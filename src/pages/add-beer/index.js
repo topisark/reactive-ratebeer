8ba71efa75b0bbe50689cf63bdea221a7b043c1c
topisark <topi.sarkkinen@gmail.com>
2018-02-01 20:55:23 +0200

    Basic layout for adding beer

--- src/pages/add-beer/index.js
+++ src/pages/add-beer/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Button, TextField } from 'material-ui';
 import './add-beer.scss';
 
 export default class AddBeerPage extends React.Component {
   render() {
     return (
       <div className="add-beer">
-        Add beer
+        <h1>Add a beer!</h1>
+        <div className="add-beer-fields">
+          <div className="add-beer-fields-input">
+            <TextField label="Name" />
+          </div>
+          <div className="add-beer-fields-input">
+            <TextField label="Brewery" />
+          </div>
+          <div className="add-beer-fields-input">
+            <TextField multiline label="Description" />
+          </div>
+        </div>
+        <Button raised color="primary">Submit the beer!</Button>
       </div>
     );
   }
